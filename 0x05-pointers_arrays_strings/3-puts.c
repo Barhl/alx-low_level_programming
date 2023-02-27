d81ae3876f4d9cb5a689261cc6c7bc52c5699451
Barhl <Alekybarlfours17@gmail.com>
2023-02-27 07:23:23 -0800

    I do not fear computers

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts - prints a string
  * @str: the string
@@ -9,7 +10,7 @@ void _puts(char *str)
 {
 	int i = 0;
 
-	while (str[i];
+	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
